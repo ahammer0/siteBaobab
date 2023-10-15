3817afc17e28d1a31a8b5c6fa6edbb7a240d5a8f
ahammer0 <axel.schwindenhammer@gmail.com>
2023-10-15 14:24:24 +0200

    ajout de puces dans la gallerie

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const btnSuivant=document.getElementById("gallerie-btnSuivant");
 const btnPrecedant=document.getElementById("gallerie-btnPrecedant");
 
-const photos=document.querySelectorAll(".gallerie img");
+const photos=document.querySelectorAll(".imageGallerie");
 let elementAffiche=0;
 function actualiserAffichage(noElement, photos){
+    const divPuces=document.querySelector(".gallerie-puces");
+    divPuces.innerHTML="";
+
     for (let i=0; i<photos.length;i++){
         if (i===noElement){
             photos[i].classList.remove("gallerie-hidden");
+            const span=document.createElement("span");
+            span.innerText="\u2022";
+            span.classList.add("gallerie-puceActive");
+            divPuces.appendChild(span);
         }
         else{
             photos[i].classList.add("gallerie-hidden");
+            const span=document.createElement("span");
+            span.innerText="\u2022";
+            span.classList.add("gallerie-puceNotActive");
+            divPuces.appendChild(span);
         }
     }
 }
