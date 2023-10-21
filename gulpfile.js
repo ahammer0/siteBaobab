1b0b35a9792dec18a3e58c1160f614b52ad9f4b3
ahammer0 <axel.schwindenhammer@gmail.com>
2023-10-21 16:02:05 +0200

    ajout d\'un linter

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass')(require('sass'));
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var sass = require("gulp-sass")(require("sass"));
 
-gulp.task('sass', function(){
-    return gulp.src("./*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./"))
-        .pipe(browserSync.stream());
+gulp.task("sass", function(){
+	return gulp.src("./*.scss")
+		.pipe(sass())
+		.pipe(gulp.dest("./"))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('serve', gulp.series('sass',function(){
-    browserSync.init({
-        server: "./"
-    });
+gulp.task("serve", gulp.series("sass",function(){
+	browserSync.init({
+		server: "./"
+	});
 
-    gulp.watch("*.scss", gulp.series('sass'));
-    gulp.watch("*.html").on('change', browserSync.reload);
+	gulp.watch("*.scss", gulp.series("sass"));
+	gulp.watch("*.html").on("change", browserSync.reload);
 }));
 
-gulp.task('default', gulp.series('serve'));
+gulp.task("default", gulp.series("serve"));
--- script.js
+++ script.js
@@ -5,59 +5,59 @@ const btnPrecedant=document.querySelector(".gallerie__btn--Precedant");
 const photos=document.querySelectorAll(".gallerie__images");
 let elementAffiche=0;
 function actualiserAffichage(noElement, photos){
-    const divPuces=document.querySelector(".gallerie__puces");
-    divPuces.innerHTML="";
+	const divPuces=document.querySelector(".gallerie__puces");
+	divPuces.innerHTML="";
 
-    for (let i=0; i<photos.length;i++){
-        if (i===noElement){
-            photos[i].classList.remove("gallerie__images--hidden");
-            const span=document.createElement("span");
-            span.innerText="\u2022";
-            span.classList.add("gallerie__puces--Active");
-            divPuces.appendChild(span);
-        }
-        else{
-            photos[i].classList.add("gallerie__images--hidden");
-            const span=document.createElement("span");
-            span.innerText="\u2022";
-            span.classList.add("gallerie__puces--NotActive");
-            divPuces.appendChild(span);
-        }
-    }
+	for (let i=0; i<photos.length;i++){
+		if (i===noElement){
+			photos[i].classList.remove("gallerie__images--hidden");
+			const span=document.createElement("span");
+			span.innerText="\u2022";
+			span.classList.add("gallerie__puces--Active");
+			divPuces.appendChild(span);
+		}
+		else{
+			photos[i].classList.add("gallerie__images--hidden");
+			const span=document.createElement("span");
+			span.innerText="\u2022";
+			span.classList.add("gallerie__puces--NotActive");
+			divPuces.appendChild(span);
+		}
+	}
 }
 actualiserAffichage(elementAffiche,photos);
 //gestion du bouton suivant
 btnSuivant.addEventListener("click",()=>{
-    if (elementAffiche<photos.length-1){
-        elementAffiche++;
-    }
-    actualiserAffichage(elementAffiche,photos);
+	if (elementAffiche<photos.length-1){
+		elementAffiche++;
+	}
+	actualiserAffichage(elementAffiche,photos);
 });
 //gestion du bouton précédent
 btnPrecedant.addEventListener("click",()=>{
-    if(elementAffiche>0){
-        elementAffiche--;
-    }
-    actualiserAffichage(elementAffiche,photos);
+	if(elementAffiche>0){
+		elementAffiche--;
+	}
+	actualiserAffichage(elementAffiche,photos);
 });
 
 //precedant en appuyant sur n
 //
 const gallerie=document.querySelector(".gallerie");
 gallerie.addEventListener("keydown",(e)=>{
-    if(e.key==="n"){
-        if (elementAffiche<photos.length-1){
-            elementAffiche++;
-        }
-        actualiserAffichage(elementAffiche,photos);
-    }
+	if(e.key==="n"){
+		if (elementAffiche<photos.length-1){
+			elementAffiche++;
+		}
+		actualiserAffichage(elementAffiche,photos);
+	}
 });
 //precedant en appuyant sur p
 gallerie.addEventListener("keydown",(e)=>{
-    if(e.key==="p"){
-        if(elementAffiche>0){
-            elementAffiche--;
-        }
-        actualiserAffichage(elementAffiche,photos);
-    }
+	if(e.key==="p"){
+		if(elementAffiche>0){
+			elementAffiche--;
+		}
+		actualiserAffichage(elementAffiche,photos);
+	}
 });
